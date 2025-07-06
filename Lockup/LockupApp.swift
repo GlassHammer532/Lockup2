ea9d03cd32b876f0231a5e0d473a553de08408f3
Felix Clissold <felixclissold2@gmail.com>
2025-07-06 19:18:45 +0100

    Initial Commit

---
+++ Lockup/LockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockupApp.swift
+//  Lockup
+//
+//  Created by Felix Clissold on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LockupTests/LockupTests.swift
@@ -0,0 +1,17 @@
+//
+//  LockupTests.swift
+//  LockupTests
+//
+//  Created by Felix Clissold on 06/07/2025.
+//
+
+import Testing
+@testable import Lockup
+
+struct LockupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
